3db2d608f17cec7abddc1fdbc6387d97d324ac84
Ankur Sheel <ankur.sheel@gmail.com>
2013-10-31 18:40:06 +0530

    1) updated jars to nightly build

--- HelloWorld-desktop/src/com/me/mygdxgame/Main.java
+++ HelloWorld-desktop/src/com/me/mygdxgame/Main.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "HelloWorld";
-		cfg.useGL20 = true;
-		cfg.width = 800;
-		cfg.height = 480;
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
 		
 		new LwjglApplication(new HelloWorld(), cfg);
 	}
